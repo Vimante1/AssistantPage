b3ca83d7c273a73d2abe0b1aa81c520c910c42a9
Vimante1 <Vimanter27@gmail.com>
2023-04-04 08:20:11 +0300

    SiteV1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using AssistantPage.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AssistantPage.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        [Route("/assistant")]
+        public ViewResult Home ()
+        {
+           
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿function onEntry(entry) {
+    console.log("asda");
+    entry.forEach(change => {
+
+        if (change.isIntersecting) {
+            change.target.classList.add('visible');
+        }
+    });
+}
+console.log("asda");
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.triger');
+for (let elm of elements) {
+    observer.observe(elm);
+}
+
+
+
+var navbarLinks = document.querySelectorAll('.nav a');
+for (var i = 0; i < navbarLinks.length; i++) {
+    navbarLinks[i].addEventListener('click', function (e) {
+        e.preventDefault(); // Отменяем стандартное поведение ссылки
+        var targetId = this.getAttribute('href');
+        var targetPosition = document.querySelector(targetId).offsetTop - 50;
+        window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth' // Добавляем плавную прокрутку
+
+        });
+    });
+}
